0aee3acd77badf07b26afc9e91350ef847e2a894
Hidayat arghandabi <hidayatarg@gmail.com>
2018-08-24 02:53:46 +0300

    error handler added

--- index.js
+++ index.js
@@ -4,33 +4,43 @@ const https= require('https');
 //getStock('USD');
 
 function getStock(stockName){
-    const request = https.get(`https://www.doviz.com/api/v1/currencies/${stockName}/latest`, response => {
-        //check if the service is working or not 
-     /*   console.log(response.statusCode);
+<<<<<<< HEAD
+    
+=======
+    try {
+>>>>>>> 9b0ffdfbf71be44a9e27c18b554b26a54353cdac
+        const request = https.get(`https://wwwdoviz.com/api/v1/currencies/${stockName}/latest`, response => {
+            //check if the service is working or not 
+            /*   console.log(response.statusCode);
         if (response.statusCode===200){
             console.log("Service is Working Properly");
         }
 */
-        //const blast the error
-        let body = '';
-        // Read the repsonse
-        response.on('data', data=>{
-           body+=data.toString();
+            //const blast the error
+            let body = '';
+            // Read the repsonse
+            response.on('data', data => {
+                body += data.toString();
 
-        });
-        
-        // Parse the data
-        response.on('end', ()=>{
-            const stock= JSON.parse(body);
-            // Read the properties
-          //   console.dir(stock);
+            });
 
-            // Print to Screen
-          //  console.log(`${stockName}(${stock.code})=>> Selling: ${stock.selling}     Buying: ${stock.buying}`);
-            printStock(stockName, stock.code, stock.selling, stock.buying);
-        });
+            // Parse the data
+            response.on('end', () => {
+                const stock = JSON.parse(body);
+                // Read the properties
+                //   console.dir(stock);
 
-    });
+                // Print to Screen
+                //  console.log(`${stockName}(${stock.code})=>> Selling: ${stock.selling}     Buying: ${stock.buying}`);
+                printStock(stockName, stock.code, stock.selling, stock.buying);
+            });
+
+            
+
+            });
+    request.on("error", error => console.error(`the problem is with${error.message}`));
+    
+    
 }
 
 function printStock(stockName, stockCode,selling, buying){
