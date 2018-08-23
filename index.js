9b0ffdfbf71be44a9e27c18b554b26a54353cdac
Hidayat arghandabi <hidayatarg@gmail.com>
2018-08-23 19:53:50 +0300

    try catch error handling

--- index.js
+++ index.js
@@ -4,33 +4,44 @@ const https= require('https');
 //getStock('USD');
 
 function getStock(stockName){
-    const request = https.get(`https://www.doviz.com/api/v1/currencies/${stockName}/latest`, response => {
-        //check if the service is working or not 
-     /*   console.log(response.statusCode);
+    try {
+        const request = https.get(`https://wwwdoviz.com/api/v1/currencies/${stockName}/latest`, response => {
+            //check if the service is working or not 
+            /*   console.log(response.statusCode);
         if (response.statusCode===200){
             console.log("Service is Working Properly");
         }
 */
-        //const blast the error
-        let body = '';
-        // Read the repsonse
-        response.on('data', data=>{
-           body+=data.toString();
+            //const blast the error
+            let body = '';
+            // Read the repsonse
+            response.on('data', data => {
+                body += data.toString();
+
+            });
+
+            // Parse the data
+            response.on('end', () => {
+                const stock = JSON.parse(body);
+                // Read the properties
+                //   console.dir(stock);
+
+                // Print to Screen
+                //  console.log(`${stockName}(${stock.code})=>> Selling: ${stock.selling}     Buying: ${stock.buying}`);
+                printStock(stockName, stock.code, stock.selling, stock.buying);
+            });
+
+            response.on('error', error => {
+                console.error(`Problem with the ${error.message}`);
+
+            });
 
         });
+    } catch (error) {
+        console.error(error.message);
         
-        // Parse the data
-        response.on('end', ()=>{
-            const stock= JSON.parse(body);
-            // Read the properties
-          //   console.dir(stock);
-
-            // Print to Screen
-          //  console.log(`${stockName}(${stock.code})=>> Selling: ${stock.selling}     Buying: ${stock.buying}`);
-            printStock(stockName, stock.code, stock.selling, stock.buying);
-        });
-
-    });
+    }
+    
 }
 
 function printStock(stockName, stockCode,selling, buying){
